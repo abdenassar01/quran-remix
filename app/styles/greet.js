86ecd69d18295af1a804e10e11a8a6f3c524a9e9
abdenassar01 <abdenassaramimi99@gmail.com>
2022-07-13 23:38:30 +0100

    adding styled components to project

---
+++ app/styles/greet.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled("div")`
+    display: flex;
+    justify-content: center;
+    align-items: centreder;
+    border: 1px solid purple;
+`
+
+export const Header = styled.h1`
+    color: red;
+`
